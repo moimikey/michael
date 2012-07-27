30da07aff1d207d2c1b243943811fb7425c37eb5
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-07-27 18:56:11 -0400

    html5 boilerplateisize

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <!doctype html>
-<html lang="en">
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-
-  <title>Backbone Boilerplate</title>
-
-  <!-- Application root. -->
-  <base href="/">
-
-  <!-- Application styles. -->
-  <link rel="stylesheet" href="assets/css/index.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<base href="/">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="assets/css/index.css">
 </head>
-
 <body>
-  <!-- Main container. -->
-  <div role="main" id="main"></div>
+	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
-  <!-- Application source. -->
-  <script data-main="app/config" src="assets/js/libs/require.js"></script>
+	<header></header>
+
+	<div role="main">
+		Hello
+	</div>
+
+	<footer></footer>
+
+	<script data-main="app/config" src="assets/js/libs/require.js"></script>
 </body>
 </html>
